1203b9bac103e4eca45af71f8e8b905cb281b085
LaughingLove <jokelovehate@gmail.com>
2018-07-12 11:45:47 -0400

    Added directory checking support (see readme)

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Exists
-A python script to check whether a file exists or not. I know I need this file a lot so I uploaded it to Github for others!
+A python script to check whether a file or directory exists or not. I know I need this file a lot so I uploaded it to Github for others!
 
 ## Installation
 Open up terminal in the project folder and just do:
@@ -12,12 +12,15 @@ That's it! You will now be able to type the command `exists.py` anywhere on the 
 
 `exists.py [path-to-file]`
 
+OR
+
+`exists.py [path-to-directory]`
+
 You can add more then one file by separating each path by a space!
 
-`exists.py [path-to-file] [path-to-second-file]`
+`exists.py [path-to-file-or-dir] [path-to-second-file-or-dir]`
 
 
 ### TODO
 
 - Add windows support (sorry)
-- Add directory checking support.
--- exists.py
+++ exists.py
@@ -9,21 +9,21 @@ def main():
     try:
         args[1]
     except IndexError:
-        print("exists.py [path-to-file]")
+        print("exists.py [path-to-file-or-directory]")
         sys.exit(1)
-    # sfile = str(args[1])
+
     # Help argument
     if str(args[1]) == "-h":
-        print("exists.py [path-to-file]")
+        print("exists.py [path-to-file-or-directory]")
         sys.exit(1)
     
     # Checks if file exists (LOL)
     counter = 0
     for arg in args:
-        if arg == "exists.py" and counter is 0:
+        if "exists.py" in arg and counter is 0:
             pass
         else:
-            if not os.path.isfile(arg):
+            if not os.path.exists(arg):
                 print("{} doesn't exist!".format(arg))
             else:
                 print("{} exists!".format(arg))
